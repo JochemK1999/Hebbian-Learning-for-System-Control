4c22c7896461451d28a2f06dd18e6c66ec66fdf9
Jochem Kerssies <jochem.kerssies@home.nl>
2024-04-04 11:55:19 +0200

    sdfg

--- chromedino.py
+++ chromedino.py
@@ -18,7 +18,7 @@ SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-NETWORK_PLAY = False
+NETWORK_PLAY = True
 
 pygame.display.set_caption("Chrome Dino Runner")
 
--- networkTrainer.py
+++ networkTrainer.py
@@ -10,11 +10,18 @@ if __name__ == "__main__":
     inputs = df.iloc[:, :-1].values  # Extract all rows and all but the last column
     outputs = df.iloc[:, -1].values  # Extract all rows and the last column
 
+    filtered_inputs = []
+    filtered_outputs = []
+    for input, output in zip(inputs, outputs):
+        if(sum(input) > 0):
+            filtered_inputs.append(input)
+            filtered_outputs.append(output)
+
     # Create a network with 30 inputs and 3 outputs
     network = HebbianNetwork.from_dimensions(inputs.shape[1]*3, 3)
 
     # Train the network
-    network.train(inputs, outputs)
+    network.train(filtered_inputs, filtered_outputs)
 
     # Save the network
     network.save('network.npy')
